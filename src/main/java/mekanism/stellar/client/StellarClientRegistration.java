5a00900a7876278b33acdc5c91fb9fc3c0cd946e
Pascalllite <pascalllite@gmail.com>
2024-12-05 16:29:05 +0900

    永久熱熱発電機を実装

--- src/main/java/mekanism/stellar/client/StellarClientRegistration.java
+++ src/main/java/mekanism/stellar/client/StellarClientRegistration.java
@@ -1,12 +1,19 @@
 package mekanism.stellar.client;
 
+import mekanism.api.providers.IBlockProvider;
 import mekanism.client.ClientRegistrationUtil;
+import mekanism.common.inventory.container.tile.MekanismTileContainer;
+import mekanism.stellar.client.gui.GuiCompressedEternalHeatGenerator;
 import mekanism.stellar.client.gui.GuiEternalHeatGenerator;
 import mekanism.stellar.client.gui.GuiStellarGenerator;
+import mekanism.stellar.common.CompressedEternalHeatGenerators;
 import mekanism.stellar.common.Stellar;
 import mekanism.stellar.common.registries.StellarBlocks;
 import mekanism.stellar.common.registries.StellarContainerTypes;
+import mekanism.stellar.common.tile.TileEntityCompressedEternalHeatGenerator;
 import net.minecraft.client.renderer.RenderType;
+import net.minecraft.network.chat.Component;
+import net.minecraft.world.entity.player.Inventory;
 import net.minecraft.world.inventory.MenuType;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.event.RegistryEvent;
@@ -15,6 +22,9 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Mod.EventBusSubscriber(modid = Stellar.MODID, value = Dist.CLIENT, bus = Mod.EventBusSubscriber.Bus.MOD)
 public class StellarClientRegistration {
     private StellarClientRegistration() {
@@ -22,13 +32,22 @@ public class StellarClientRegistration {
 
     @SubscribeEvent
     public static void init(FMLClientSetupEvent event) {
-        ClientRegistrationUtil.setRenderLayer(renderType -> renderType == RenderType.solid() || renderType == RenderType.translucent(), StellarBlocks.STELLAR_GENERATOR);
+        List<IBlockProvider> objects = new ArrayList<>();
+        objects.add(StellarBlocks.STELLAR_GENERATOR);
+        objects.add(StellarBlocks.ETERNAL_HEAT_GENERATOR);
+        for (CompressedEternalHeatGenerators type : CompressedEternalHeatGenerators.values()) {
+            objects.add(StellarBlocks.COMPRESSED_ETERNAL_HEAT_GENERATORS.get(type));
+        }
+        ClientRegistrationUtil.setRenderLayer(renderType -> renderType == RenderType.solid() || renderType == RenderType.translucent(), objects.toArray(IBlockProvider[]::new));
     }
 
     @SubscribeEvent(priority = EventPriority.LOW)
     public static void registerContainers(RegistryEvent.Register<MenuType<?>> event) {
         ClientRegistrationUtil.registerScreen(StellarContainerTypes.STELLAR_GENERATOR, GuiStellarGenerator::new);
         ClientRegistrationUtil.registerScreen(StellarContainerTypes.ETERNAL_HEAT_GENERATOR, GuiEternalHeatGenerator::new);
+        for (CompressedEternalHeatGenerators type : CompressedEternalHeatGenerators.values()) {
+            ClientRegistrationUtil.registerScreen(StellarContainerTypes.COMPRESSED_ETERNAL_HEAT_GENERATORS.get(type), (MekanismTileContainer<TileEntityCompressedEternalHeatGenerator> container, Inventory inv, Component title) -> new GuiCompressedEternalHeatGenerator(container, inv, title, type));
+        }
     }
 
 }
--- src/main/java/mekanism/stellar/common/registries/StellarBlocks.java
+++ src/main/java/mekanism/stellar/common/registries/StellarBlocks.java
@@ -5,9 +5,16 @@ import mekanism.common.item.block.machine.ItemBlockMachine;
 import mekanism.common.registration.impl.BlockDeferredRegister;
 import mekanism.common.registration.impl.BlockRegistryObject;
 import mekanism.generators.common.content.blocktype.Generator;
+import mekanism.stellar.common.CompressedEternalHeatGenerators;
 import mekanism.stellar.common.Stellar;
+import mekanism.stellar.common.tile.TileEntityCompressedEternalHeatGenerator;
 import mekanism.stellar.common.tile.TileEntityEternalHeatGenerator;
 import mekanism.stellar.common.tile.TileEntityStellarGenerator;
+
+import java.util.EnumMap;
+import java.util.function.UnaryOperator;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class StellarBlocks {
     public static final BlockDeferredRegister BLOCKS = new BlockDeferredRegister(Stellar.MODID);
@@ -15,6 +22,8 @@ public class StellarBlocks {
     public static final BlockRegistryObject<BlockTileModel<TileEntityStellarGenerator, Generator<TileEntityStellarGenerator>>, ItemBlockMachine> STELLAR_GENERATOR = BLOCKS.register("stellar_generator", () -> new BlockTileModel<>(StellarBlockTypes.STELLAR_GENERATOR), ItemBlockMachine::new);
 
     public static final BlockRegistryObject<BlockTileModel<TileEntityEternalHeatGenerator, Generator<TileEntityEternalHeatGenerator>>, ItemBlockMachine> ETERNAL_HEAT_GENERATOR = BLOCKS.register("eternal_heat_generator", () -> new BlockTileModel<>(StellarBlockTypes.ETERNAL_HEAT_GENERATOR), ItemBlockMachine::new);
+
+    public static final EnumMap<CompressedEternalHeatGenerators, BlockRegistryObject<BlockTileModel<TileEntityCompressedEternalHeatGenerator, Generator<TileEntityCompressedEternalHeatGenerator>>, ItemBlockMachine>> COMPRESSED_ETERNAL_HEAT_GENERATORS = Stream.of(CompressedEternalHeatGenerators.values()).collect(Collectors.toMap(UnaryOperator.identity(), type -> BLOCKS.register(type.path(), () -> new BlockTileModel<>(StellarBlockTypes.COMPRESSED_ETERNAL_HEAT_GENERATORS.get(type)), ItemBlockMachine::new), (a, b) -> a, () -> new EnumMap<>(CompressedEternalHeatGenerators.class)));
 
     private StellarBlocks() {
     }
